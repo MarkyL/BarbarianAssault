107b3af0d48daa338c80e421eb0224c1c8de64ee
MarkyL <mark.lu91@gmail.com>
2019-07-06 17:08:13 +0300

    - A null pointer check because the tower btn might have not been chosen.

--- BarbarianAssault/Assets/Scripts2D/TowerManager.cs
+++ BarbarianAssault/Assets/Scripts2D/TowerManager.cs
@@ -20,7 +20,7 @@ public class TowerManager : Singleton<TowerManager>
     void Update()
     {
         //If the left mouse button is clicked.
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && towerBtnPressed != null)
         {
             //Get the mouse position on the screen and send a raycast into the game world from that position.
             Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
